0b22b990f20fb60c1facbfe153d17d9031aa1f87
PapelConPera <lionel.copajira@gmail.com>
2024-12-10 19:46:03 -0300

    agregando una tabla

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -1,14 +1,20 @@
+import Link from "next/link";
 import { ModeToggle } from "../theme-toggle-button";
+import { buttonVariants } from "./button";
 
 function Navbar(){
     return(
         <nav className="flex justify-between">
-            <h1>
-                NextActionsCRUD
-            </h1>
+            <h1>NextActionsCRUD</h1>
 
+<div className="flex gap-x-2 items-">
+    <Link href="/new"
+    className={buttonVariants({variant: "outline"})}>
+        Create Task
+    </Link>
+        <ModeToggle />
 
-            <ModeToggle />
+</div>
         </nav>
     )
 }
